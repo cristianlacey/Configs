fcf8c32987b65c9f9d57096dc2c4843f9e7d7255
Cristian Lacey <clacey@princeton.edu>
2018-12-19 15:01:13 -0500

    Modified setup script to filter comments in packages.list

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
 # Install packages in packages.list
-cat packages.list | xargs sudo apt-get -y install
+cat packages.list | grep -v -E "#|^$" | xargs sudo apt-get -y install
+
+# To check for packages installed with apt, execute the following:
+# zgrep 'Commandline: apt' /var/log/apt/history.log /var/log/apt/history.log.*.gz

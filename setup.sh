19c855594e2ca9f7729198724b8636918bd325ad
Cristian Lacey <clacey@princeton.edu>
2018-12-24 17:00:34 -0500

    Added gdebi-core to packages and setup now downloads atom

--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@ sudo apt-get update
 # Install packages in packages.list
 cat packages.list | grep -v -E "#|^$" | xargs sudo apt-get -y install
 
+# Install Atom
+wget -O atom-amd64.deb https://atom.io/download/deb
+sudo gdebi atom-amd64.deb
+rm atom-amd64.deb
+
 # To check for packages installed with apt, execute the following:
 # zgrep 'Commandline: apt' /var/log/apt/history.log /var/log/apt/history.log.*.gz

04167ee40114960313a85fbc1792f006efd91a78
Cristian Lacey <clacey@princeton.edu>
2019-02-12 09:15:49 -0500

    Changed link_new_configs.sh to use absolute paths, so it can be run from Configs

--- link_new_configs.sh
+++ link_new_configs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Run this script to remove config files currently in home directory
-# and create hard links to files in ~/Configs
+# and create hard links to files in ~/Configs.
 
-rm .bashrc
-rm .hgrc
-rm .emacs
-rm .gitconfig
+rm ~/.bashrc
+rm ~/.hgrc
+rm ~/.emacs
+rm ~/.gitconfig
 
-ln ~/Configs/bashrc .bashrc
-ln ~/Configs/hgrc .hgrc
-ln ~/Configs/emacs .emacs
-ln ~/Configs/gitconfig .gitconfig
+ln ~/Configs/bashrc ~/.bashrc
+ln ~/Configs/hgrc ~/.hgrc
+ln ~/Configs/emacs ~/.emacs
+ln ~/Configs/gitconfig ~/.gitconfig

144178793317832ab79205ada5c2aed603939210
Cristian Lacey <clacey@princeton.edu>
2019-03-04 20:35:52 -0500

    added dedicated python_packages

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sudo apt-get update
 # Install packages in packages.list
 cat packages.list | grep -v -E "#|^$" | xargs sudo apt-get -y install
 
+# Install packages in python_packages.list
+cat python_packages.list | grep -v -E "#|^$" | xargs pip3 install
+
 # Install Atom
 wget -O atom-amd64.deb https://atom.io/download/deb
 sudo gdebi atom-amd64.deb
